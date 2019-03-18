79423c628ed5da852ea09285d29c04d0b00b57bc
Harald Grove <haraldgrove@gmail.com>
2019-03-18 17:01:07 +0700

    Updated alignment script

--- 01_Alignment.sh
+++ 01_Alignment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Usage: 01_alignment.sh <sample_name>
+
+# Input: FASTQ-files on the form <sample_name>_R1.fastq.gz, <sample_name>_R2.fastq.gz
+# Requires: samtools v.1.7
+#           docker image biocontainers/bwa
+
 dir_fastq=PATH_TO/fastq
 dir_Output=PATH_TO/results
 dir_Hg38=PATH_TO/hg38bundle
@@ -9,9 +15,9 @@ mkdir -p ${dir_Output}/$1/{LOG,BAM}
 
 docker run --rm -v ${dir_fastq}:/fastq -v ${dir_Hg38}:/reference biocontainers/bwa \
 	bwa mem \
-	-t 16 \
+	-t 4 \
 	-R "@RG\tID:$1\tSM:$1\tPL:Illumina\tLB:WES" \
 	/reference/Homo_sapiens_assembly38.fasta.gz \
-	/fastq/$1_Sample_$2/$2_combined_R1.fastq.gz \
-	/fastq/$1_Sample_$2/$2_combined_R2.fastq.gz \
+	/fastq/$1_R1.fastq.gz \
+	/fastq/$1_R2.fastq.gz \
 	| samtools sort -o ${dir_Output}/$1/BAM/$1_sorted.bam
