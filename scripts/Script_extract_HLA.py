069302491135ba4cae5ae840386e1369d9262113
Harald Grove <haraldgrove@gmail.com>
2019-04-25 10:20:41 +0700

    Bugfixes, added filter to Kourami prediction

--- scripts/Script_extract_HLA.py
+++ scripts/Script_extract_HLA.py
@@ -9,13 +9,11 @@ from collections import Counter
 #sample=BTN-034
 
 sample = sys.argv[1]
-input_file = '06_EpitopePrediction.sh.template'
-HLA_file_kourami = '{}.result'.format(sample)
-#HLA_file_phlat = '{}_HLA.sum'.format(sample)
-HLA_file_phlat = "/root/Somatic/data/Output/BTN_034/PHLAT/BTN_034_HLA.sum"
-#HLA_file_hlahd = '{}_final.result.txt'.format(sample)
-HLA_file_hlahd = "/root/Somatic/data/Output/BTN_034/HLAHD/BTN_034_final.result.txt"
-script_file = '/root/script/06_Pvac_{}.sh'.format(sample)
+input_file = sys.argv[2] # script template
+HLA_file_kourami = sys.argv[3]
+HLA_file_phlat = sys.argv[4]
+HLA_file_hlahd = sys.argv[5]
+script_file = '06_Pvac_{}.sh'.format(sample)
 
 #HLA LIST
 hla_kourami = []
@@ -31,20 +29,22 @@ with open(HLA_file_kourami, 'r') as fin:
             allele = l[0]
         except IndexError:
             continue
+        if ';' in allele:
+            continue
         if allele[:2] in ['A*','B*','C*']:
             hla_result = allele.split(':')
             t = 'HLA-' + hla_result[0]+':' + hla_result[1]
             if t not in hla_kourami:
                 hla_kourami.append(t)
             hla_ALL.append(t)
-        if allele[:5] in ['DRB1*','DQA1*','DQB1*']:
+        elif allele[:5] in ['DRB1*','DQA1*','DQB1*']:
             hla_result = allele.split(':')
             t = hla_result[0]+ ':' + hla_result[1]
             if t not in hla_kourami:
                 hla_kourami.append(t)
             hla_ALL.append(t)
 
-#print(hla_kourami)
+#print('kourami=', hla_kourami)
 
 ##############PHLAT##############
 
@@ -67,7 +67,7 @@ with open(HLA_file_phlat, 'r') as fin:
             if t_2 not in hla_phlat:
                 hla_phlat.append(t_2)
             hla_ALL.append(t_2)
-        if allele_1[:5] in ['DRB1*','DQA1*','DQB1*']:
+        elif allele_1[:5] in ['DRB1*','DQA1*','DQB1*']:
             hla_result_1 = allele_1.split(':')
             t_1 = hla_result_1[0] +':'+ hla_result_1[1]
             if t_1 not in hla_phlat:
@@ -79,7 +79,7 @@ with open(HLA_file_phlat, 'r') as fin:
                 hla_phlat.append(t_2)
             hla_ALL.append(t_2)
 
-#print(hla_phlat)
+#print('phlat=', hla_phlat)
 
 ##############HLAHD##############
 
@@ -103,7 +103,7 @@ with open(HLA_file_hlahd, 'r') as fin:
             if t_2 not in hla_hlahd:
                 hla_hlahd.append(t_2)
             hla_ALL.append(t_2)
-        if allele_0 in ['DRB1','DQA1','DQB1']:
+        elif allele_0 in ['DRB1','DQA1','DQB1']:
             hla_result_1 = allele_1.split(':')
             t_1 = (hla_result_1[0])[4:]+':' + hla_result_1[1]
             if t_1 not in hla_hlahd:
@@ -115,31 +115,31 @@ with open(HLA_file_hlahd, 'r') as fin:
                 hla_hlahd.append(t_2)
             hla_ALL.append(t_2)
 
-#print(hla_hlahd)
-#print(hla_ALL)
+#print('hlahd=', hla_hlahd)
+#print('all=', hla_ALL)
 Dict_ALL = (dict((x,hla_ALL.count(x)) for x in set(hla_ALL)))
 #print(Dict_ALL)
 
-A=[]
-B=[]
-C=[]
-DQB1=[]
-DQA1=[]
-DRB1=[]
+A = []
+B = []
+C = []
+DQB1 = []
+DQA1 = []
+DRB1 = []
 
 for i in hla_ALL:
     hla_class = i.split('*')[0]
     if hla_class == "HLA-A":
         A.append(i)
-    if hla_class == "HLA-B":
+    elif hla_class == "HLA-B":
         B.append(i)
-    if hla_class == "HLA-C":
+    elif hla_class == "HLA-C":
         C.append(i)
-    if hla_class == "DQB1":
+    elif hla_class == "DQB1":
         DQB1.append(i)
-    if hla_class == "DQA1":
+    elif hla_class == "DQA1":
         DQA1.append(i)
-    if hla_class == "DRB1":
+    elif hla_class == "DRB1":
         DRB1.append(i)
 
 Dict_A = (dict((x,A.count(x)) for x in set(A)))
@@ -158,30 +158,27 @@ SD_DRB1 = sorted(Dict_DRB1.items(), key=lambda kv: kv[1], reverse=True)
 
 LIST_ALL=[]
 
-for hla_class in [SD_A,SD_B,SD_C,SD_DQB1,SD_DQA1,SD_DRB1]:
+for hla_class in [SD_A, SD_B, SD_C, SD_DQB1, SD_DQA1, SD_DRB1]:
     N = len(hla_class)
-    if (hla_class)[0][1] >= 5:
+    if (hla_class)[0][1] >= 5: # 11 11 12
         LIST_ALL.append(hla_class[0][0])
-        continue
-    if (hla_class)[0][1] == 4:
+    elif (hla_class)[0][1] == 4: # 11 12 12 or 11 12 13
+        LIST_ALL.append(hla_class[0][0])
         if (hla_class)[1][1] == 2:
-            LIST_ALL.append(hla_class[0][0])
             LIST_ALL.append(hla_class[1][0])
-            continue
-    if (hla_class)[0][1] == 3:
+    elif (hla_class)[0][1] == 3:
         if (hla_class)[1][1] == 3 or (hla_class)[1][1] == 2:
             LIST_ALL.append(hla_class[0][0])
             LIST_ALL.append(hla_class[1][0])
-            continue     
-    else:
-        for each in range(N) :
-            LIST_ALL.append(hla_class[each-1][0])
+        else:
+            for each in range(N) :
+                LIST_ALL.append(hla_class[each-1][0])
 
 print(LIST_ALL)
 
 with open(input_file, 'r') as fin, open(script_file, 'w') as fout:
     for line in fin:
-        if line.startswith'#HLA=':
+        if line.startswith('#HLA='):
             fout.write('HLA={}\n'.format(','.join(LIST_ALL)))
         elif line.startswith('#Abnm_ID='):
             fout.write('Abnm_ID={}\n'.format(sample))
