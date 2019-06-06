691ac997d65a9c821171dd9bd173387af93a71e1
Harald Grove <haraldgrove@gmail.com>
2019-06-06 16:59:57 +0700

    Improved logic for merging HLA-predctions

--- scripts/parse_HLA.py
+++ scripts/parse_HLA.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import numpy as np
 import re
+from operator import itemgetter
+from collections import Counter
 
 #############KOURAMI##############
 def kourami(infile):
@@ -19,9 +21,14 @@ def kourami(infile):
             if gene in ['A','B','C']:
                 gene = 'HLA-' + gene
             if gene not in db:
-                db[gene] = {0:'',1:'',2:''}
+                db[gene] = {0:'',1:'',2:[]}
             if ';' in allele:
-                db[gene][2] += allele
+                for al in allele.split(';'):
+                    gene = re.search('[^*]+',al).group(0)
+                    if gene in ['A','B','C']:
+                        gene = 'HLA-' + gene
+                    fields = re.search('..:[^:]+',al).group(0)
+                    db[gene][2].append(fields)
             elif db[gene][0] == '':
                 db[gene][0] = fields
             else:
@@ -57,11 +64,10 @@ def hlahd(infile):
     db = {}
     with open(infile, 'r') as fin:
         for line in fin:
-            l = line.strip().split()
+            l = line.strip().split('\t')
             try:
                 gene = l[0]
-                allele_1 = l[1]
-                allele_2 = l[2]
+                alleles = l[1:]
             except IndexError:
                 continue
             if gene not in ['A','B','C','DQA1','DQB1','DRB1']:
@@ -69,141 +75,119 @@ def hlahd(infile):
             if gene in ['A','B','C']:
                 gene = 'HLA-' + gene
             if gene not in db:
-                db[gene] = {0:'',1:''}
-            try:
-                fields = re.search('..:[^:]+',allele_1).group(0)
-            except AttributeError:
-                fields = ''
-            db[gene][0] = fields
-            try:
-                fields = re.search('..:[^:]+',allele_2).group(0)
-            except AttributeError:
-                fields = ''
-            db[gene][1] = fields
+                db[gene] = {0:'',1:'', 2:[]}
+            if len(alleles) > 2:
+                first, second = [],[]
+                for ind, allele in enumerate(alleles):
+                    if ind % 2 == 0:
+                        first.append(allele)
+                    else:
+                        second.append(allele)
+                fir = Counter(first).most_common()[0]
+                sec = Counter(second).most_common()[0]
+                if fir[1] > len(first) // 2:
+                    try:
+                        fields = re.search('..:[^:]+',fir[0]).group(0)
+                    except AttributeError:
+                        fields = ''
+                    db[gene][0] = fields
+                    if sec[1] > len(second)//2:
+                        try:
+                            fields = re.search('..:[^:]+',sec[0]).group(0)
+                        except AttributeError:
+                            fields = ''
+                        db[gene][1] = fields
+                    else:
+                        for secon in second:
+                            try:
+                                fields = re.search('..:[^:]+',secon).group(0)
+                            except AttributeError:
+                                fields = ''
+                            db[gene][2].append(fields)
+                else:
+                    for al in alleles:
+                        try:
+                            fields = re.search('..:[^:]+',al).group(0)
+                        except AttributeError:
+                            fields = ''
+                        db[gene][2].append(fields)
+            else:
+                try:
+                    fields = re.search('..:[^:]+',alleles[0]).group(0)
+                except AttributeError:
+                    fields = ''
+                db[gene][0] = fields
+                try:
+                    fields = re.search('..:[^:]+',alleles[1]).group(0)
+                except AttributeError:
+                    fields = ''
+                db[gene][1] = fields
     return db
 
-def combine(k1,k2,ke,p1,p2,h1,h2):
-    first, second = '',''
-    if k1 != '' and (k1 in [p1,p2] or k1 in [h1,h2]):
-        first = k1
-        if p1 == k1:
-            p1 = ''
-        elif p2 == k1:
-            p2 = ''
-        if h1 == k1:
-            h1 = ''
-        elif h2 == k1:
-            h2 = ''
-        k1 = ''
-    elif k2 != '' and (k2 in [p1,p2] or k2 in [h1,h2]):
-        first = k2
-        if p1 == k2:
-            p1 = ''
-        elif p2 == k2:
-            p2 = ''
-        if h1 == k2:
-            h1 = ''
-        elif h2 == k2:
-            h2 = ''
-        k2 = ''
-    elif p1 != '' and p1 in [h1,h2]:
-        first = p1
-        if h1 == p1:
-            h1 = ''
-        elif h2 == p1:
-            h2 = ''
-        p1 = ''
-    elif p2 != '' and p2 in [h1,h2]:
-        first = p2
-        if h1 == p2:
-            h1 = ''
-        elif h2 == p2:
-            h2 = ''
-        p2 = ''
-    if k1 != '' and (k1 in [p1,p2] or k1 in [h1,h2]):
-        second = k1
-        if p1 == k1:
-            p1 = ''
-        elif p2 == k1:
-            p2 = ''
-        if h1 == k1:
-            h1 = ''
-        elif h2 == k1:
-            h2 = ''
-        k1 = ''
-    elif k2 != '' and (k2 in [p1,p2] or k2 in [h1,h2]):
-        second = k2
-        if p1 == k2:
-            p1 = ''
-        elif p2 == k2:
-            p2 = ''
-        if h1 == k2:
-            h1 = ''
-        elif h2 == k2:
-            h2 = ''
-        k2 = ''
-    elif p1 != '' and p1 in [h1,h2]:
-        second = p1
-        if h1 == p1:
-            h1 = ''
-        elif h2 == p1:
-            h2 = ''
-        p1 = ''
-    elif p2 != '' and p2 in [h1,h2]:
-        second = p2
-        if h1 == p2:
-            h1 = ''
-        elif h2 == p2:
-            h2 = ''
-        p2 = ''
-    return first, second
+def combine(algo):
+    """ algo is a list of algorithm outputs, i.e. lists of the predicted alleles from each software  """
+    alleles = set([a for li in algo for a in li])
+    res = []
+    for allele in alleles:
+        if allele == '':
+            continue
+        count1, count2 = 0, 0
+        for group in algo:
+            temp = group.count(allele)
+            if temp > 0:
+                count1 += 1
+            if temp > 1:
+                count2 += 1
+        res.append([allele,count1])
+        res.append([allele,count2])
+    res = sorted(res, key=itemgetter(1), reverse=True)
+    if len(res) == 0 or res[0][1] <= len(algo) // 2: # No data, or no agreement
+        first, second = 'NaN','NaN'
+        return first, second
+    first = res[0][0]
+    if res[1][1] <= len(algo) // 2: # Only first allele has an agreement
+        second = 'NaN'
+    else:
+        second = res[1][0]
+    return first,second
 
 x1, x2, x3, x4, x5 = [], [], [], [], []
-for index in range(34,93):
-    sample = 'BTN-0{}'.format(index)
-    genes = ['HLA-A','HLA-B','HLA-C','DQA1','DQB1','DRB1']
-    infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN-0{}.result'.format(index)
-    try:
-        ko = kourami(infile)
-    except FileNotFoundError:
-        continue
-    infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN_0{}_PHLAT_final.result.txt'.format(index)
-    ph = phlat(infile)
-    infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN_0{}_HLAHD_final.result.txt'.format(index)
-    hl = hlahd(infile)
-    for gene in genes:
+outfile = '/home/harald/projects/somatic/test_samples/hla_alleles/HLA_alleles_prediction.txt'
+with open(outfile, 'w') as fout:
+    for index in range(34,93):
+        sample = 'BTN-0{}'.format(index)
+        genes = ['HLA-A','HLA-B','HLA-C','DQA1','DQB1','DRB1']
+        infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN-0{}.result'.format(index)
         try:
-            k1, k2, ke = ko[gene][0], ko[gene][1], ko[gene][2]
-        except KeyError:
-            k1, k2, ke = '','',''
-        try:
-            p1, p2 = ph[gene][0], ph[gene][1]
-        except KeyError:
-            p1, p2 = '', ''
-        try:
-            h1, h2 = hl[gene][0], hl[gene][1]
-        except KeyError:
-            h1, h2 = '', ''
-        first, second = combine(k1, k2, ke, p1, p2, h1, h2)
-        try:
-            a1,a2 = first.split(':')
-        except ValueError:
-            a1, a2 = np.nan,np.nan
-        if gene.startswith('HLA-'):
-            gene = gene[4:]
-        x1.append(sample)
-        x2.append(gene)
-        x3.append(1)
-        x4.append(a1)
-        x5.append(a2)
-        try:
-            b1,b2 = second.split(':')
-        except ValueError:
-            b1,b2 = np.nan,np.nan
-        x1.append(sample)
-        x2.append(gene)
-        x3.append(2)
-        x4.append(b1)
-        x5.append(b2)
-
-df = pd.DataFrame({'Sample':x1,'Gene':x2,'Allele':x3,'Field1':x4,'Field2':x5})
+            ko = kourami(infile)
+        except FileNotFoundError:
+            continue
+        infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN_0{}_PHLAT_final.result.txt'.format(index)
+        ph = phlat(infile)
+        infile = '/home/harald/projects/somatic/test_samples/hla_alleles/BTN_0{}_HLAHD_final.result.txt'.format(index)
+        hl = hlahd(infile)
+        for gene in genes:
+            try:
+                k1, k2, ke = ko[gene][0], ko[gene][1], ko[gene][2]
+            except KeyError:
+                k1, k2, ke = '','',[]
+            try:
+                p1, p2 = ph[gene][0], ph[gene][1]
+            except KeyError:
+                p1, p2 = '', ''
+            try:
+                h1, h2, he = hl[gene][0], hl[gene][1], hl[gene][2]
+            except KeyError:
+                h1, h2, he = '', '', []
+            first, second = combine([[k1, k2]+ke, [p1, p2], [h1, h2]+he])
+            source = [k1,k2]+ke+[p1,p2]+[h1,h2]+he
+            extra = [element for element in source if element != '' and element != first]
+            if first != 'NaN':
+                first = gene + '*' + first
+            if second != 'NaN':
+                second = gene + '*' + second
+            if first == 'NaN' or second == 'NaN':
+                fout.write('{}\t{}\t{}\t[{}]\n'.format(sample,first,second,';'.join(extra)))
+            else:
+                fout.write('{}\t{}\t{}\n'.format(sample,first,second))
+            
