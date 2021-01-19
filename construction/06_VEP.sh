d406ba773695ccd6efb354c3814a4aa6efae2d5f
Harald Grove <haraldgrove@gmail.com>
2021-01-19 10:18:23 +0700

    Remove outdated files

--- construction/06_VEP.sh
+++
@@ -1,5 +0,0 @@
-./vep \
-	--input_file /Output/$1/$1_m2.vcf.gz --output_file /Output/$1/$1_m2_vep.vcf \
-	--format vcf --vcf --symbol --terms SO --tsl \
-	--offline --cache \
-	--plugin Downstream --plugin Wildtype --force_overwrite
